1a83df20cead3fdc91db6f7970c803975460c63d
Sam Salisbury <samsalisbury@gmail.com>
2023-07-21 08:40:34 +0100

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,8 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
-        pages: [
+        page_url: 'https://www.notion.so/Notion-DoIt-fffab2a6442643f6985c3f0a0ddf14c4?pvs=4',
             // {
             //     page_id: '<page_id>',
             //     target_folder: 'path/relative/to/content/folder'
